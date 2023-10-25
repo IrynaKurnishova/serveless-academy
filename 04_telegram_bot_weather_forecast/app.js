0266a95d1810844febe6287f04b83d8e61d0d3be
irinakurnishova <kurnishovaiv@gmail.com>
2023-10-25 17:51:39 +0200

    corrections and exclude .env from git

--- 04_telegram_bot_weather_forecast/app.js
+++ 04_telegram_bot_weather_forecast/app.js
@@ -1,97 +1,59 @@
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios')
 const token = process.env.TOKEN;
-const mapboxAPIKEY = process.env.API_KEY;
-const cityName = process.env.CITY;
 const bot = new TelegramBot(token, { polling: true });
+const {getSearchResults, responseToUser} = require('./utils')
 
-const keyboard = {
+const keyboardStart = {
     reply_markup: {
         keyboard: [
-            [{ text: 'Forecast in Kharkiv'}],
+            [{ text: 'Forecast in Kharkiv' }],
         ],
         one_time_keyboard: true,
     },
 };
 
-const keyboardIntervals =  {
+const keyboardIntervals = {
     reply_markup: {
         keyboard: [
             [{ text: 'At intervals of 3 hours' }],
             [{ text: 'At intervals of 6 hours' }]
         ],
         one_time_keyboard: true,
-    }
-}
-
-const now = new Date();
-const stringDate = `${now}\n`;
-const messageDate = stringDate.replace("GMT+0200", "")
-const twentyFourHoursLater = new Date(now.getTime() + 24 * 60 * 60 * 1000);
-
-const responseToUser = (mainMessage) => {
-    return `<b>Weather in Kharkiv for the next 24 hours</b>\n\n${messageDate}\n${mainMessage}`
-}
-
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    if (msg.text === '/start') bot.sendMessage(chatId, 'Choose forecast', keyboard)
-});
-
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-    const result = await getSearchResults();
-
-    if (text === 'Forecast in Kharkiv') {
-        bot.sendMessage(chatId, 'Choose interval', keyboardIntervals);
-
-    } else if (text === 'At intervals of 3 hours') {
-
-        const mainMessage = result
-            .map((data) => {
-                return `Time: ${data.time.split(" ")[1]}\nTemperature (°C): ${data.temperature}\nPrecipitation: ${data.precipitation}\n\n`;
-            }).join('\n');
-        const response = responseToUser(mainMessage)
-        bot.sendMessage(chatId, response, { parse_mode: 'HTML' });
-
-    } else if (text === 'At intervals of 6 hours') {
-        const mainMessage = result
-            .filter((data, index) => index % 2 === 0)
-            .map((data) => {
-                return `Time: ${data.time.split(" ")[1]}\nTemperature (°C): ${data.temperature}\nPrecipitation: ${data.precipitation}\n\n`;
-            })
-            .join('\n');
-        const response = responseToUser(mainMessage)
-        bot.sendMessage(chatId, response, { parse_mode: 'HTML' });
-    }
-});
-
-
-const getSearchResults = async () => {
-    try {
-        const result = await axios.get(
-            `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${mapboxAPIKEY}`
-        );
-
-        if (result.data.list) {
-            const next24HoursData = result.data.list.filter(item => {
-                const itemDate = new Date(item.dt_txt);
-                return itemDate >= now && itemDate <= twentyFourHoursLater;
-            });
-
-            const filteredTimeAndTemp = next24HoursData.map(item => ({
-                time: item.dt_txt.replace(":00", ""),
-                temperature: Math.round(item.main.temp - 273.15),
-                precipitation: item.weather[0]?.description,
-            }));
-
-            return filteredTimeAndTemp;
-        } else {
-            return [];
-        }
-    } catch (error) {
-        return {error: "An error occurred while creating the report"}
-    }
+    },
 };
+
+const commandActions = {
+    'Forecast in Kharkiv': sendIntervalChoice,
+    'At intervals of 3 hours': sendWeatherMessage(3),
+    'At intervals of 6 hours': sendWeatherMessage(6)
+};
+
+function sendIntervalChoice(chatId) {
+    bot.sendMessage(chatId, 'Choose interval', keyboardIntervals);
+}
+
+function sendWeatherMessage(interval) {
+    return async (chatId) => {
+        const result = await getSearchResults();
+        const filteredData = interval === 6 ? result.filter((_, index) => index % 2 === 0) : result;
+        const mainMessage = filteredData.map((data) => {
+            return `Time: ${data.time.split(" ")[1]}\nTemperature: ${data.temperature}\nPrecipitation: ${data.precipitation}\n\n`
+        }).join('\n');
+        const response = responseToUser(mainMessage);
+        bot.sendMessage(chatId, response, { parse_mode: 'HTML' });
+    }
+}
+
+bot.onText(/\/start/, ({ chat }) => {
+    const chatId = chat.id;
+    bot.sendMessage(chatId, 'Choose forecast', keyboardStart);
+});
+
+bot.on('message', async ({ chat, text }) => {
+    const chatId = chat.id;
+    if (commandActions[text]) {
+        commandActions[text](chatId)
+    }
+});
+
 
